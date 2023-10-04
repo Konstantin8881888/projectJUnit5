57d99ab9aca8279fcde6bda5ebf0aeb31dc59545
Konstantin8881888 <kamagoroff@gmail.com>
2023-10-05 00:34:13 +0300

    First tests cheking

---
+++ src/main/java/xyz/belochka/junit/service/UserService.java
@@ -0,0 +1,18 @@
+package xyz.belochka.junit.service;
+
+import xyz.belochka.junit.dto.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    private final List<User> users = new ArrayList<>();
+
+    public List<User> getAll(){
+        return users;
+    }
+
+    public boolean add(User user) {
+        return users.add(user);
+    }
+}
--- src/test/java/xyz/belochka/junit/service/UserServiceTest.java
+++ src/test/java/xyz/belochka/junit/service/UserServiceTest.java
@@ -1,11 +1,47 @@
 package xyz.belochka.junit.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import xyz.belochka.junit.dto.User;
 
-public class UserServiceTest {
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserServiceTest {
+    private static UserService userService;
+
+    @BeforeAll
+    void initAll(){
+        System.out.println("Before all " + this);
+        userService = new UserService();
+    }
+    @BeforeEach
+    void prepare(){
+        System.out.println("Before each " + this);
+    }
     @Test
-    void test(){
-        Assertions.assertTrue(false);
+    void usersEmptyIfNotAdded(){
+        System.out.println("Test 1 " + this);
+        List<User> users = userService.getAll();
+        assertTrue(users.isEmpty());
+    }
+
+    @Test
+    void usersSizeIfUserAdded(){
+        System.out.println("Test 2 " + this);
+        userService.add(new User());
+        userService.add(new User());
+        List<User> users = userService.getAll();
+        assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    void deleteData(){
+        System.out.println("After each " + this);
+    }
+    @AfterAll
+    void deleteAll(){
+        System.out.println("After all " +this);
     }
 }
