3538d70a5745d68ad39b807fc717071b5e6848c4
Konstantin8881888 <kamagoroff@gmail.com>
2023-10-05 15:33:27 +0300

    Added tags and refactor structure

--- src/test/java/xyz/belochka/junit/TestLauncher.java
+++ src/test/java/xyz/belochka/junit/TestLauncher.java
@@ -3,6 +3,7 @@ package xyz.belochka.junit;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TagFilter;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
@@ -16,6 +17,7 @@ public class TestLauncher {
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
                 .request()
                 .selectors(DiscoverySelectors.selectPackage("xyz.belochka.junit.service"))
+//                .filters(TagFilter.includeTags("Login"))
                 .build();
         launcher.execute(request, summaryGeneratingListener);
         try (PrintWriter writer = new PrintWriter(System.out)) {
--- src/test/java/xyz/belochka/junit/service/UserServiceTest.java
+++ src/test/java/xyz/belochka/junit/service/UserServiceTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestMethodOrder(MethodOrderer.MethodName.class)
 class UserServiceTest {
     private static final User VASYA = User.of(1, "Vasya", "123");
     private static final User FEDYA = User.of(2, "Fedya", "1234");
@@ -20,84 +21,82 @@ class UserServiceTest {
     void initAll() {
         System.out.println("Before all " + this);
     }
-
     @BeforeEach
     void prepare() {
         System.out.println("Before each " + this);
         userService = new UserService();
     }
-
-    @Test
-    void usersEmptyIfNotAdded() {
-        System.out.println("Test 1 " + this);
-        List<User> users = userService.getAll();
-        assertTrue(users.isEmpty());
-    }
-
-    @Test
-    void usersSizeIfUserAdded() {
-        System.out.println("Test 2 " + this);
-        userService.add(VASYA, FEDYA);
-        List<User> users = userService.getAll();
-        assertThat(users).hasSize(2);
+    @Tag("user")
+    @Nested
+    class UserTests {
+        @Test
+        void usersEmptyIfNotAdded() {
+            System.out.println("Test 1 " + this);
+            List<User> users = userService.getAll();
+            assertTrue(users.isEmpty());
+        }
+        @Test
+        void usersSizeIfUserAdded() {
+            System.out.println("Test 2 " + this);
+            userService.add(VASYA, FEDYA);
+            List<User> users = userService.getAll();
+            assertThat(users).hasSize(2);
 //        assertEquals(2, users.size());
+        }
+        @Test
+        void usersConvertedToMapById() {
+            userService.add(VASYA, FEDYA);
+            Map<Integer, User> users = userService.getAllConverted();
+            assertAll(
+                    () -> assertThat(users).containsKeys(VASYA.getId(), FEDYA.getId()),
+                    () -> assertThat(users).containsValues(VASYA, FEDYA)
+            );
+        }
     }
-
-    @Test
-    void loginSuccesIfUserExist() {
-        userService.add(VASYA);
-        Optional<User> maybeUser = userService.login(VASYA.getUsername(), VASYA.getPassword());
-        assertThat(maybeUser).isPresent();
-        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(VASYA));
+    @Tag("login")
+    @Nested
+    class LoginTests {
+        @Test
+        void loginSuccesIfUserExist() {
+            userService.add(VASYA);
+            Optional<User> maybeUser = userService.login(VASYA.getUsername(), VASYA.getPassword());
+            assertThat(maybeUser).isPresent();
+            maybeUser.ifPresent(user -> assertThat(user).isEqualTo(VASYA));
 //        assertTrue(maybeUser.isPresent());
 //        maybeUser.ifPresent(user -> assertEquals(VASYA,user));
-    }
-
-    @Test
-    void usersConvertedToMapById() {
-        userService.add(VASYA, FEDYA);
-        Map<Integer, User> users = userService.getAllConverted();
-        assertAll(
-                () -> assertThat(users).containsKeys(VASYA.getId(), FEDYA.getId()),
-                () -> assertThat(users).containsValues(VASYA, FEDYA)
-        );
-
-    }
-
-    @Test
-    void loginFailIfPasswordNotCorrect() {
-        userService.add(VASYA);
-        Optional<User> maybeUser = userService.login(VASYA.getUsername(), "Incorrect");
-        assertTrue(maybeUser.isEmpty());
-    }
-
-    @Test
-    void loginFailIfUserDoesNotExist() {
-        userService.add(VASYA);
-        Optional<User> maybeUser = userService.login("Incorrect", VASYA.getPassword());
-        assertTrue(maybeUser.isEmpty());
-    }
-
-    @Test
-    void throwExeptionIfLoginOrPasswordNull() {
-        assertAll(
-                () -> {
-                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> userService.login(null, "Incorrect"));
-                    assertThat(exception.getMessage()).isEqualTo("Username or password is null");
-                },
-                () -> {
-                    IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> userService.login("Incorrect", null));
-                    assertThat(exception1.getMessage()).isEqualTo("Username or password is null");
-                }
-        );
+        }
+        @Test
+        void loginFailIfPasswordNotCorrect() {
+            userService.add(VASYA);
+            Optional<User> maybeUser = userService.login(VASYA.getUsername(), "Incorrect");
+            assertTrue(maybeUser.isEmpty());
+        }
+        @Test
+        void loginFailIfUserDoesNotExist() {
+            userService.add(VASYA);
+            Optional<User> maybeUser = userService.login("Incorrect", VASYA.getPassword());
+            assertTrue(maybeUser.isEmpty());
+        }
+        @Test
+        void throwExeptionIfLoginOrPasswordNull() {
+            assertAll(
+                    () -> {
+                        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> userService.login(null, "Incorrect"));
+                        assertThat(exception.getMessage()).isEqualTo("Username or password is null");
+                    },
+                    () -> {
+                        IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> userService.login("Incorrect", null));
+                        assertThat(exception1.getMessage()).isEqualTo("Username or password is null");
+                    }
+            );
 //        assertThrows(IllegalArgumentException.class, () -> userService.login(null, "Incorrect"));
+        }
     }
 
     @AfterEach
     void deleteData() {
         System.out.println("After each " + this);
     }
-
     @AfterAll
     void deleteAll() {
         System.out.println("After all " + this);
